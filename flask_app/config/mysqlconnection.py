252a1b16b7d296822606648a76b616444b92d0b9
Juan Barrezueta <78634162+Juanron23@users.noreply.github.com>
2023-01-17 20:30:18 -0500

    Update mysqlconnection.py

--- flask_app/config/mysqlconnection.py
+++ flask_app/config/mysqlconnection.py
@@ -5,8 +5,8 @@ class MySQLConnection:
     def __init__(self, db):
         # change the user and password as needed
         connection = pymysql.connect(host = 'localhost',
-                                    user = 'root', 
-                                    password = '8232', 
+                                    user = '#####', 
+                                    password = '#####', 
                                     db = db,
                                     charset = 'utf8mb4',
                                     cursorclass = pymysql.cursors.DictCursor,
